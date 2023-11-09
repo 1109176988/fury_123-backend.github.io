5340e8b44861ae8cabb82d21dbb89176ca459473
liuao <13082636+liu_ao_admin@user.noreply.gitee.com>
2023-11-09 16:28:45 +0800

    编辑用户列表界面

--- src/views/system/AdminUser.vue
+++ src/views/system/AdminUser.vue
@@ -18,7 +18,7 @@ import { TableInstance, buildTable } from '@/types/table'
 import api from '@/api'
 
 // 使用 models
-import { OptionItem, AdminListArgs, AdminItem } from 'models'
+import { OptionItem, AdminListArgs, AdminItem, AdminSetRoleArgs, AdminSetPwdArgs, AdminCreateArgs } from 'models'
 
 // 设置 角色 下拉框数据字段
 const roleList = ref<OptionItem[]>([])
@@ -36,7 +36,6 @@ const roleOpt = ref<TableInstance<number>>()
         console.log(roleOpt.value, 'roleOpt.value')
     }).catch(api.errorPage)
 }
-
 
 // 设置
 
@@ -75,17 +74,134 @@ function submitSearch() {
     params.status = searchFormState.status
     params.username = searchFormState.username
     params.page = 1
-
+    queryData()
 }
 
-import { FormInstance } from 'element-plus';
-
+import { FormInstance, ElMessage } from 'element-plus';
 
 // 链接 el-form 元素
 const searchFormRef = ref<FormInstance>()
 
+// 启用/禁用 按钮逻辑
+function setStatus(id: number, status: number, idx: number) {
+    api.adminSetStatus.do({ id, status: status, }, loading).then(() => {
+        tableData.value[idx].status = status
+        ElMessage.success('操作成功')
+    }).catch(api.errorMsg)
+}
+
+// 设置 index
+let index = -1
+
+// 设置 用户名字段
+const user = ref('')
+
+// el-drawer 显示隐藏
+const roleVisible = ref(false)
+
+// 设置el-form的节点
+const roleFormRef = ref<FormInstance>()
+
+// el-form 数据
+const roleFormState = reactive<AdminSetRoleArgs>({
+    id: 0,
+    role_id: 0,
+})
+
+// 点击 变更角色
+function openRole(item: AdminItem, idx: number) {
+    index = idx
+    user.value = item.username
+    roleFormState.id = item.id
+    roleFormState.role_id = item.role_id
+    roleVisible.value = true
+}
+
+// 关闭 el-drawer
+function closeRole() {
+    roleVisible.value = false
+}
 
 
+// el-form 点击确定提交
+function submitRole() {
+    roleFormRef.value?.validate((valid) => {
+        if (!valid) return false
+        api.adminSetRole.do(roleFormState, loading).then(() => {
+            tableData.value[index].role_id = roleFormState.role_id
+            ElMessage.success('变更成功')
+            closeRole()
+        }).catch(api.errorMsg)
+    })
+}
+
+// el-drawer 重置密码 显示隐藏
+const pwdVisible = ref(false)
+
+// 点击 重置密码
+function openPwd(item: AdminItem) {
+    user.value = item.username
+    pwdFormState.id = item.id
+    pwdVisible.value = true
+}
+// 重置密码表单节点
+const pwdFormRef = ref<FormInstance>()
+
+// 重置密码表单数据
+const pwdFormState = reactive<AdminSetPwdArgs>({
+    id: 0,
+    password: ''
+})
+
+// 
+import rand from '@/utils/rand'
+
+// 重置密码表单确定按钮
+function submitPwd() {
+    pwdFormRef.value?.validate((valid) => {
+        if (!valid) return false
+        api.adminSetPwd.do(pwdFormState, loading).then(() => {
+            ElMessage.success('重置成功')
+            closePwd()
+        }).catch(api.errorMsg)
+    })
+}
+
+
+// 关闭重置密码表单
+function closePwd() {
+    pwdVisible.value = false
+}
+
+// 新增用户表单显示/隐藏
+const addVisible = ref(false)
+
+// 新增用户表单数据
+const addFormState = reactive<AdminCreateArgs>({
+    username: '',
+    password: '',
+    role_id: 0,
+})
+
+// 创建新增的el-form的节点
+const addFormRef = ref<FormInstance>()
+
+// 关闭新增表单
+function closeAdd() {
+    addVisible.value = false
+}
+// 新增用户表单确定按钮
+function submitAdd() {
+    addFormRef.value?.validate((valid) => {
+        if (!valid) return false
+        api.adminCreate.do(addFormState, loading).then(() => {
+            ElMessage.success('创建成功')
+            searchFormRef.value?.resetFields()
+            submitSearch()
+            closeAdd()
+        }).catch(api.errorMsg)
+    })
+}
 </script>
 
 <template>
@@ -138,7 +254,7 @@ const searchFormRef = ref<FormInstance>()
                 <template #header>
                     <el-space class="justify">
                         <span>操作</span>
-                        <el-button link type="primary" size="small">+ 新增</el-button>
+                        <el-button link type="primary" size="small" @click="addVisible = true">+ 新增</el-button>
                     </el-space>
                 </template>
 
@@ -148,7 +264,7 @@ const searchFormRef = ref<FormInstance>()
                         <template v-for="item in $table.instance.status.options()" :key="item.value">
                             <el-popconfirm v-if="row.status !== item.value && api.adminSetStatus.permit()"
                                 :title="`确定要${$table.instance.status.render(item.value)}账号 「 ${row.username} 」吗? `"
-                                cancel-button-type="default" width="auto" @confirm="">
+                                cancel-button-type="default" width="auto" @confirm="setStatus(row.id, item.value, $index)">
                                 <template #reference>
                                     <el-button :type="$table.map.status[item.value]" size="small">
                                         {{ $table.instance.status.render(item.value) }}
@@ -156,8 +272,10 @@ const searchFormRef = ref<FormInstance>()
                                 </template>
                             </el-popconfirm>
                         </template>
-                        <el-button type="primary" size="small" @click="">变更角色</el-button>
-                        <el-button type="warning" size="small" @click="">重置密码</el-button>
+                        <el-button type="primary" size="small" @click="openRole(row, $index)"
+                            v-if="api.adminSetRole.permit()">变更角色</el-button>
+                        <el-button type="warning" size="small" @click="openPwd(row)"
+                            v-if="api.adminSetPwd.permit()">重置密码</el-button>
                     </span>
                 </template>
             </el-table-column>
@@ -165,16 +283,78 @@ const searchFormRef = ref<FormInstance>()
 
         <!-- 分页组件 -->
         <el-pagination small v-model:current-page="params.page" v-model:page-size="params.size"
-            :page-sizes="[10, 20, 50, 100]" layout="total,prev,pager,next,sizes,jumper" :total="total" @current-change=""
-            @size-change=""></el-pagination>
+            :page-sizes="[10, 20, 50, 100]" layout="total,prev,pager,next,sizes,jumper" :total="total"
+            @current-change="queryData" @size-change="queryData">
+        </el-pagination>
 
 
 
-        <!-- el-drawer -->
-        <el-drawer>
-
+        <!-- el-drawer 变更角色 -->
+        <el-drawer v-model="roleVisible" @close="roleFormRef?.resetFields()" title="变更角色">
+            <el-form ref="roleFormRef" v-loading="loading" label-width="80px" :model="roleFormState">
+                <el-form-item label="用户名">
+                    <span>{{ user }}</span>
+                </el-form-item>
+                <el-form-item label="角色" prop="role_id"
+                    :rules="[{ required: true, type: 'number', min: 1, message: '请选择角色!' }]">
+                    <el-select v-model="roleFormState.role_id" filterable style="width: 100%;">
+                        <el-option :value="0" label="请选择" disabled></el-option>
+                        <el-option v-for="item in roleList" :key="item.id" :label="item.name" :value="item.id"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="submitRole">确定</el-button>
+                    <el-button @click="closeRole()">取消</el-button>
+                </el-form-item>
+            </el-form>
         </el-drawer>
 
+        <!-- el-drawer 重置密码 -->
+        <el-drawer v-model="pwdVisible" @close="pwdFormRef?.resetFields()" title="重置密码">
+            <el-form ref="pwdFormRef" :model="pwdFormState" v-loading="loading" label-width="80px">
+                <el-form-item label="用户名">
+                    <span>{{ user }}</span>
+                </el-form-item>
+                <el-form-item label="新密码" prop="password" :rules="[{ require: true, min: 8, message: '请输入至少8字符的新密码' }]">
+                    <el-input v-model="pwdFormState.password" :maxlength="33">
+                        <template #append>
+                            <el-button @click="pwdFormState.password = rand.strings()">生成</el-button>
+                        </template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="submitPwd">确定</el-button>
+                    <el-button @click="closePwd">取消</el-button>
+                </el-form-item>
+            </el-form>
+        </el-drawer>
+
+        <!-- el-drawer 新增用户 -->
+        <el-drawer v-model="addVisible" title="新增用户" size="500" @close="addFormRef?.resetFields()">
+            <el-form ref="addFormRef" :model="addFormState" v-loading="loading" label-width="80px">
+                <el-form-item label="用户名" prop="username"
+                    :rules="[{ required: true, min: 8, message: '请输入至少把字符的新密8字符的用户名' }]">
+                    <el-input v-model="addFormState.username" :malength="33"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="password" :rules="[{ required: true, min: 8, message: '请输入至少把字符的新密8字符的密码' }]">
+                    <el-input v-model="addFormState.password" :maxlength="33">
+                        <template #append>
+                            <el-button @click="addFormState.password = rand.strings()">生成</el-button>
+                        </template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item label="角色" prop="role_id" :rules="[{ required: true, type: 'number', min: 1, message: '请选择角色' }]">
+                    <el-select v-model="addFormState.role_id" filterable style="width:100%">
+                        <el-option :value="0" label="请选择" disabled></el-option>
+                        <el-option v-for="item in roleList" :key="item.id" :label="item.name" :value="item.id"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item>
+                    <el-button @click="submitAdd" type="primary">确定</el-button>
+                    <el-button @click="closeAdd">取消</el-button>
+                </el-form-item>
+            </el-form>
+        </el-drawer>
     </div>
 </template>
 
